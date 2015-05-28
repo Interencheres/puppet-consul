c5fdbcd910cf85613ca464d5c4aad1d7aa81eb53
Olivier <ofontannaud@cpmultimedia.com>
2015-05-28 16:05:20 +0200

    Use top scope var

--- manifests/reload.pp
+++ manifests/reload.pp
@@ -4,10 +4,12 @@
 # It will execute `consul reload`, useful for if a config file has changed.
 #
 class consul::reload {
+
   exec { 'consul reload':
     cwd         => $consul::config_dir,
     path        => [$consul::bin_dir, '/bin', '/usr/bin'],
     command     => 'consul reload',
     refreshonly => true,
   }
+
 }
